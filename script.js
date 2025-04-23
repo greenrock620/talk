589f6d4d674f98a6f849a97e88eb47a70ec018aa
greenrock620 <greenrock620@126.com>
2025-04-23 13:40:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ async function sendMessage() {
     loadingMsg.classList.add('loading-message');
 
     try {
-        const response = await fetch('https://9d56-2408-8207-6c83-1690-117-f819-efe-c225.ngrok-free.app/chat', {
+        const response = await fetch('https://29e6-2408-8207-6c83-1690-117-f819-efe-c225.ngrok-free.app/chat', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message })
